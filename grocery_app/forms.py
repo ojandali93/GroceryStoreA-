77410442a7db3c27a991efbf7a1dae89331e9ac6
omar jandali <omarjandali93@gmail.com>
2022-05-09 01:05:46 -0700

    completed the second part ofthe project

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -19,4 +19,31 @@ class GroceryItemForm(FlaskForm):
     photo_url = StringField("Photo URL: ")
     store = SelectField(u'Store', choices = [(store.id, store.title) for store in GroceryStore.query.all()])
     submit = SubmitField("Submit")
-    submit = SubmitField("Submit")
+
+class SignUpForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if not user:
+            raise ValidationError('No user with that username. Please try again.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user and not bcrypt.check_password_hash(
+                user.password, password.data):
+            raise ValidationError('Password doesn\'t match. Please try again.')
--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -5,6 +5,7 @@ from grocery_app.forms import GroceryStoreForm, GroceryItemForm
 
 # Import app and db from events_app package so that we can run app
 from grocery_app.extensions import app, db
+from flask_login import login_required, login_user, logout_user, current_user
 
 main = Blueprint("main", __name__)
 
@@ -18,55 +19,104 @@ def homepage():
     print(all_stores)
     return render_template('home.html', all_stores=all_stores)
 
+@login_required
 @main.route('/new_store', methods=['GET', 'POST'])
 def new_store():
-    # TODO: Create a GroceryStoreForm
+    form = GroceryStoreForm()
+    if request.method == 'POST':
+      store_title = request.form.get('title')
+      store_address = request.form.get('address')
+      store = GroceryStore(
+          title = store_title,
+          address = store_address,
+          created_by = current_user,
+      )
+      db.session.add(store)
+      db.session.commit()
+      return redirect(url_for('main.store_detail', store_id = store.id))
+    else:
+      return render_template('new_store.html', form=form)
 
-    # TODO: If form was submitted and was valid:
-    # - create a new GroceryStore object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the store detail page.
-
-    # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_store.html')
-
+@login_required
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
-    # TODO: Create a GroceryItemForm
+    form = GroceryItemForm()
+    if request.method == 'POST':
+        item_name = request.form.get('name')
+        item_price = request.form.get('price')
+        item_category = request.form.get('category')
+        item_photo_url = request.form.get('photo_url')
+        item_store = request.form.get('store')
+        item = GroceryItem(
+            name = item_name,
+            price = item_price,
+            category = item_category,
+            photo_url = item_photo_url,
+            store_id = item_store,
+            created_by = current_user,
+        )
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('main.item_detail', item_id=item.id))
+    else: 
+        return render_template('new_item.html', form=form)
 
-    # TODO: If form was submitted and was valid:
-    # - create a new GroceryItem object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the item detail page.
-
-    # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
-
+@login_required
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
-    # TODO: Create a GroceryStoreForm and pass in `obj=store`
+    form = GroceryStoreForm(obj=store)
+    if request.method == 'POST':
+        store.title = request.form.get('title')
+        store.address = request.form.get('address')
+        db.session.commit()
+        return redirect(url_for('main.store_detail', store_id = store.id))
+    else:
+        return render_template('store_detail.html', store=store, form=form)
 
-    # TODO: If form was submitted and was valid:
-    # - update the GroceryStore object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the store detail page.
-
-    # TODO: Send the form to the template and use it to render the form fields
-    store = GroceryStore.query.get(store_id)
-    return render_template('store_detail.html', store=store)
-
+@login_required
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
-    # TODO: Create a GroceryItemForm and pass in `obj=item`
-
-    # TODO: If form was submitted and was valid:
-    # - update the GroceryItem object and save it to the database,
-    # - flash a success message, and
-    # - redirect the user to the item detail page.
-
-    # TODO: Send the form to the template and use it to render the form fields
+    form = GroceryItemForm(obj=item)
+    if request.method == 'POST':
+        item.name = request.form.get('name')
+        item.price = request.form.get('price')
+        item.category = request.form.get('category').upper()
+        item.photo_url = request.form.get('photo_url')
+        item.store_id = request.form.get('store')
+        db.session.commit()
+        return redirect(url_for('main.item_detail', item_id = item.id))
     item = GroceryItem.query.get(item_id)
-    return render_template('item_detail.html', item=item)
+    return render_template('item_detail.html', item=item, form=form)
 
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            password=form.username.password
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    print(form.errors)
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
